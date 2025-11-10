bf2cf142ddd0e1fc6655005957dea1353fdb8536
Bikram Chidi <veekrum.thapa@gmail.com>
2025-11-10 20:13:33 +0545

    fix

--- manual_deploy.sh
+++ manual_deploy.sh
@@ -5,6 +5,6 @@ ssh  root@54.91.15.72 "mkdir -p /root/ridemio_website/ && rm -rf /root/ridemio_w
 
 tar -czf - . | ssh root@54.91.15.72 "tar -xzf - -C /root/ridemio_website/"
 
-ssh root@54.91.15.72 "cd /root/ridemio_website && /usr/local/bin/docker-compose up -d -f docker-compose-prod.yml --force-recreate --build"
+ssh root@54.91.15.72 "cd /root/ridemio_website && /usr/local/bin/docker-compose -f docker-compose-prod.yml up -d --force-recreate --build"
 
 echo " Deployment Finished "
