938524d88c43205773e8282411170c86c32e4d9f
Bikram Chidi <veekrum.thapa@gmail.com>
2025-11-10 20:09:41 +0545

    fix

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -xe
-cd /root/ridemio_website && /usr/local/bin/docker-compose up -d -f docker/docker-compose-prod.yml --force-recreate --build
+cd /root/ridemio_website && /usr/local/bin/docker-compose -f docker-compose-prod.yml up -d --force-recreate --build
