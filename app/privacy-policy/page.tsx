a6213ff8cf7a88cc9378cc582293d8a9fcb347ff
shuseelmaharjan <maharzanshuseel10@gmail.com>
2025-11-12 12:48:43 +0545

    added pages for mobile app pages about-us, privacy-policy, terms-of-use

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const AboutUsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>Welcome to our company! We are dedicated to providing the best service possible.</p>
+    </div>
+  );
+};
+export default AboutUsPage;
