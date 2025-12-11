4f031507f2d4c38e5c00f889762e048069ee8b99
Shuseel Maharjan <maharjan.shuseel@gmail.com>
2025-12-11 22:45:30 +0545

    updated curt pages design

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -25,10 +25,10 @@ export function Header() {
     return (
         <>
             <header className="sticky top-0 z-50 border-b bg-white">
-                <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 md:px-0">
+                <div className="mx-auto flex h-16 container items-center justify-between px-4 md:px-0">
                     <HeaderLogo />
 
-                    <div className="flex items-center gap-4">
+                    <div className="flex items-center">
                         <HeaderActions
                             isMobileOpen={isMobileMenuOpen}
                             toggleMobile={() => setIsMobileMenuOpen((prev) => !prev)}
