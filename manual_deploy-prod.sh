7ffb651b94e3bf130e334fde1eb3e192481cf7aa
Bikram Chidi <bikaram.chidi@hazesoft.co>
2026-02-01 16:36:27 +0545

    manual deploy

---
+++ manual_deploy-prod.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+set -xe
+
+# cat key/private_key_base | base64 -d > key/private_key
+# chmod 400 key/private_key
+PROD_IP="202.51.70.55"
+
+ssh root@$PROD_IP "mkdir -p /root/ridemio_website/ && rm -rf /root/ridemio_website/*"
+
+tar -czf - . | ssh root@$PROD_IP "tar -xzf - -C /root/ridemio_website/"
+
+ssh root@$PROD_IP "cd /root/ridemio_website && bash run-prod.sh"
+
+echo " Deployment Finished "
+
